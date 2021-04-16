0f40757829128413106d5b934f62a080c234ba13
mae beam <maebeam0@gmail.com>
2021-04-15 20:50:39 -0700

    enable auto updates and dev tools

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
 const { app, BrowserWindow } = require('electron')
 
+// setup auto updates
+require('update-electron-app')()
+
 function createWindow () {
   const win = new BrowserWindow({
     width: 1440,
     height: 900,
+    webPreferences: {
+      devTools: true,
+    }
   })
 
   win.loadURL("https://bitclout.com");
