1ca24d197296b9c931c6b865c994335be1789728
mae beam <maebeam0@gmail.com>
2021-04-19 21:43:43 -0700

    Open all links except identity in browser

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, shell } = require('electron')
 
 // setup auto updates
 require('update-electron-app')()
@@ -9,10 +9,24 @@ function createWindow () {
     height: 900,
     webPreferences: {
       devTools: true,
+      contextIsolation: false,
     }
   })
 
   win.loadURL("https://bitclout.com");
+
+  win.on('page-title-updated', (evt) => {
+    evt.preventDefault();
+  });
+
+  win.webContents.on('new-window', function(e, url) {
+    if (url.startsWith('https://identity.bitclout.com')) {
+      return;
+    }
+
+    e.preventDefault();
+    shell.openExternal(url);
+  });
 }
 
 app.whenReady().then(() => {
